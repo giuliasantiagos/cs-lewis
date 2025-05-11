c2ce00b1fc9fb772931d59e0c384c8a17e1fe806
giuliasantiagos <giulia.silva@sptech.school>
2025-05-11 12:15:15 -0300

    adicionando api web-data-viz

---
+++ website/public/js/script-cadastro.js
@@ -0,0 +1,132 @@
+// Array para armazenar usuários cadastrados
+let listaUsuariosCadastrados = [];
+
+function cadastrar() {
+
+  //Recupere o valor da nova input pelo nome do id
+  // Agora vá para o método fetch logo abaixo
+
+  var nomeVar = ipt_nome.value;
+  var emailVar = ipt_email.value;
+  var senhaVar = ipt_senha.value;
+  var confsenhaVar = ipt_confirme_senha.value;
+
+  // Verificando se há algum campo em branco
+  if (
+    nomeVar == "" ||
+    emailVar == "" ||
+    senhaVar == "" ||
+    confsenhaVar == ""
+
+  ) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(Mensagem de erro para todos os campos em branco)";
+
+    finalizarAguardar();
+    return false;
+  } else {
+    setInterval(sumirMensagem, 5000);
+  }
+
+  // Verificando se o email já foi cadastrado
+  for (let i = 0; i < listaUsuariosCadastrados.length; i++) {
+    if (!listaUsuariosCadastrados.includes(emailVar)) {
+      console.log("E-mail válido.");
+      break;
+    } else {
+      cardErro.style.display = "block";
+      mensagem_erro.innerHTML = "(Mensagem de erro para e-mail inválido)";
+      finalizarAguardar();
+    }
+  }
+
+  // Verificando se o nome possui mais de uma letra:
+  if (nomeVar.length <= 1) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(Mensagem de erro para nome inválido.)";
+    finalizarAguardar();
+    return false;
+  } else {
+    setInterval(sumirMensagem, 5000);
+  }
+
+  // Verificando se o email é válido:
+  if (!emailVar.includes('@') || !emailVar.includes('.')) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(Mensagem de erro para email inválido.)";
+    finalizarAguardar();
+    return false;
+  } else {
+    setInterval(sumirMensagem, 5000);
+  }
+
+    // Verificando se a senha é válida:
+  if (senhaVar.length <= 1) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(Mensagem de erro para senha inválida.)";
+    finalizarAguardar();
+    return false;
+  } else {
+    setInterval(sumirMensagem, 5000);
+  }
+
+    // Verificando se a confirmação de senha é válida:
+  if ((confsenhaVar.length <= 1) || senhaVar != confsenhaVar) {
+    cardErro.style.display = "block";
+    mensagem_erro.innerHTML =
+      "(Mensagem de erro para senha inválida.)";
+    finalizarAguardar();
+    return false;
+  } else {
+    setInterval(sumirMensagem, 5000);
+  }
+
+  // Enviando o valor da nova input
+  fetch("/usuario/cadastrar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // crie um atributo que recebe o valor recuperado aqui
+      // Agora vá para o arquivo routes/usuario.js
+      nomeServer: nomeVar,
+      emailServer: emailVar,
+      senhaServer: senhaVar,
+      confsenhaServer: confsenhaVar
+    }),
+  })
+    .then(function (resposta) {
+      console.log("resposta: ", resposta);
+
+      if (resposta.ok) {
+        cardErro.style.display = "block";
+
+        mensagem_erro.innerHTML =
+          "Cadastro realizado com sucesso! Redirecionando para tela de login...";
+
+        setTimeout(() => {
+          window.location = "../html/login.html";
+        }, "2000");
+
+        limparFormulario();
+        finalizarAguardar();
+      } else {
+        throw "Houve um erro ao tentar realizar o cadastro!";
+      }
+    })
+    .catch(function (resposta) {
+      console.log(`#ERRO: ${resposta}`);
+      finalizarAguardar();
+    });
+
+  return false;
+}
+
+function sumirMensagem() {
+  cardErro.style.display = "none";
+}
---
+++ website/src/database/script-cs-lewis.sql
@@ -0,0 +1,29 @@
+create database csLivros;
+use csLivros;
+
+create table usuario(
+idusuario int primary key auto_increment,
+nome varchar(45) not null,
+email varchar(65) not null,
+senha varchar(30) not null,
+confirmarsenha varchar(30) not null
+);
+
+create table livros(
+idlivros int primary key auto_increment,
+nome varchar(65) not null,
+editora varchar(45) not null,
+publicacao date,
+genero_literario varchar(45),
+numpagina int not null,
+constraint chk_editora check (editora in ('Thomas Nelson Brasil', 'HarperCollins Brasil'))
+)auto_increment = 1001;
+
+create table usuario_livros(
+fkusuario int,
+fklivros int,
+status_leitura varchar(45),
+primary key(fkusuario, fklivros),
+constraint fk_usuario foreign key (fkusuario) references usuario(idusuario),
+constraint fk_livros foreign key (fklivros) references livros(idlivros)
+);
