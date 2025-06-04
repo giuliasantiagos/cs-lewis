51a9c862e999dbc6a39a0f22591b86fd1c99b2bf
Giulia Santiago <giulia.silva@sptech.school>
2025-06-03 21:28:12 -0300

    adicionando o BD na pasta

---
+++ website/bd/script-cs-lewis.sql
@@ -0,0 +1,18 @@
+create database csLivros;
+use csLivros;
+
+create table usuario(
+idusuario int primary key auto_increment,
+nome varchar(45) not null,
+email varchar(65) not null,
+senha varchar(30) not null,
+key(email)
+);
+
+create table usuario_livros(
+fkusuario int,
+livro_favorito varchar(60),
+genero_favorito varchar(45),
+qtdLidos int,
+constraint fk_usuario foreign key (fkusuario) references usuario(idusuario)
+);
